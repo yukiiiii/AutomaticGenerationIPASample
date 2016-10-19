d7fdbc55bab5de31a734de54978455a258a7515c
yuoku <yuoku0708@gmail.com>
2016-10-20 02:19:18 +0900

    Initial Commit

---
+++ SampleApp20161020/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleApp20161020
+//
+//  Created by OkuderaYuki on 2016/10/20.
+//  Copyright © 2016年 yuoku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleApp20161020Tests/SampleApp20161020Tests.m
@@ -0,0 +1,39 @@
+//
+//  SampleApp20161020Tests.m
+//  SampleApp20161020Tests
+//
+//  Created by OkuderaYuki on 2016/10/20.
+//  Copyright © 2016年 yuoku. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SampleApp20161020Tests : XCTestCase
+
+@end
+
+@implementation SampleApp20161020Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
